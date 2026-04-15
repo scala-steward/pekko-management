d4a7c8ddc46ee7ed16f2d2ce2336d7f49e498bf0
Scala Steward <scala_steward@virtuslab.com>
2026-04-15 14:43:38 +0000

    Update guava to 33.6.0-jre

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   val scalaTestVersion = "3.2.20"
 
   val awsSdkVersion = "1.12.797"
-  val guavaVersion = "33.5.0-jre"
+  val guavaVersion = "33.6.0-jre"
   val jacksonVersion = "2.21.2"
 
   val log4j2Version = "2.25.4"
