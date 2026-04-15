d6ba1be93d28ee5b877989693e41f85e32c29e99
Scala Steward <scala_steward@virtuslab.com>
2026-04-15 14:43:45 +0000

    Update consul-client to 1.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,7 +46,7 @@ object Dependencies {
   val discoveryConsul = Seq(
     "org.apache.pekko" %% "pekko-actor" % pekkoVersion,
     "org.apache.pekko" %% "pekko-discovery" % pekkoVersion,
-    "org.kiwiproject" % "consul-client" % "1.11.0",
+    "org.kiwiproject" % "consul-client" % "1.12.0",
     "org.testcontainers" % "testcontainers-consul" % "2.0.4" % Test,
     "org.scalatest" %% "scalatest" % scalaTestVersion % Test,
     "org.apache.pekko" %% "pekko-testkit" % pekkoVersion % Test,
